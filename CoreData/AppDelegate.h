244d715d122b80c7420109bd98d0cfa3fdf72a5e
Edward Liu <xinedwardliu@gmail.com>
2016-02-17 21:25:34 -0500

    initial

---
+++ CoreData/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CoreData
+//
+//  Created by Edward Liu on 16/1/25.
+//  Copyright © 2016年 Edward Liu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ CoreData/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  CoreData
+//
+//  Created by Edward Liu on 16/1/25.
+//  Copyright © 2016年 Edward Liu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
